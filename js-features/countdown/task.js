5764c886300b27e4c380a9bd793bf6057b2ce53e
Stanislav <pavlov-sta@mail.ru>
2019-03-03 23:19:33 +0500

    Задание 1. Задание 2 с дополнением

---
+++ js-features/countdown/task.js
@@ -0,0 +1,10 @@
+const addText = function () {
+    const output = document.getElementById("timer");
+    output.textContent -= 1;
+    
+    if (output.textContent <= 0) {
+        alert('Вы выйграли');
+        clearInterval(inervalID);
+    }
+}
+const inervalID = setInterval(addText, 100);
